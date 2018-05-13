9fbc9dbb501defaa9cbdf8333ff8b0f6902ba365
RegretDNM <33679224+RegretDNM@users.noreply.github.com>
2018-05-14 02:48:41 +0300

    Add files via upload
    Now minimax is added. Althoutgh I didn't remove the random. For now, because I think it's a useless option. Also changed start screen a little.

---
+++ src/sample/PointsAndScores.java
@@ -0,0 +1,15 @@
+package sample;
+
+/**
+ * Created by Regret on 12.05.2018.
+ */
+public class PointsAndScores {
+
+    int score;
+    Point point;
+
+    PointsAndScores(int score, Point point) {
+        this.score = score;
+        this.point = point;
+    }
+}
